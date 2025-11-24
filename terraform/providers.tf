efd5e039bf0e2c5d95ab45affbbf1f9ec83e7935
victor <victor.rascle@lyceeastier.com>
2025-11-24 13:09:22 +0100

    terraform okay

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,11 +8,27 @@ terraform {
     }
   }
 
-  backend "local" {} # pour commencer, tu peux plus tard mettre S3 backend
+  backend "local" {}
 }
-provider "null" {}
 
 provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
+  region                      = var.aws_region
+  access_key                  = "test"
+  secret_key                  = "test"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+
+  endpoints {
+    ec2            = "http://localhost:4566"
+    iam            = "http://localhost:4566"
+    s3             = "http://localhost:4566"
+    sts            = "http://localhost:4566"
+    route53        = "http://localhost:4566"
+    rds            = "http://localhost:4566"
+    lambda         = "http://localhost:4566"
+    cloudformation = "http://localhost:4566"
+    dynamodb       = "http://localhost:4566"
+  }
 }
